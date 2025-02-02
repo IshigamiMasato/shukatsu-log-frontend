5aaa55c076df3b643767a48a46c0ad245f5b487e
M I <mi@emunoMacBook-Pro.local>
2025-02-02 12:53:30 +0900

    予定登録時にメッセージを表示するようにする

--- src/app/calender/page.tsx
+++ src/app/calender/page.tsx
@@ -7,7 +7,7 @@ import { RootState } from "@/store";
 import { useRouter } from "next/navigation";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { Event } from "@/types";
+import { Event, Msg } from "@/types";
 import { EVENT_TYPES } from "@/constants/eventConstants";
 
 const Calender = () => {
@@ -15,10 +15,27 @@ const Calender = () => {
     const { isAuthenticated, user, authStatusChecked } = useSelector( (state: RootState) => state.auth );
     const router = useRouter();
 
+    // 予定登録フォームの状態管理
+    const [title, setTitle] = useState("");
+    const [type, setType] = useState("");
+    const [startAt, setStartAt] = useState("");
+    const [endAt, setEndAt] = useState("");
+    const [memo, setMemo] = useState("");
+
+    const clearForm = () => {
+        setTitle("");
+        setType("");
+        setStartAt("");
+        setEndAt("");
+        setMemo("");
+    }
+
+    const [msg, setMsg] = useState<Msg|null>();
+
     const [events, setEvents] = useState<Event[]>([]);
 
     useEffect(() => {
-        if (authStatusChecked) {
+        if ( authStatusChecked ) {
             // 認証状態確認後、未認証だった場合はログイン画面へリダイレクト
             if ( ! isAuthenticated ) {
                 router.push("/login");
@@ -52,11 +69,15 @@ const Calender = () => {
         fetch('/api/event', {method: "POST", headers: {Authorization: jwt},  body: formData})
             .then(res => {
                 if( ! res.ok ) {
+                    setMsg({status: "error", content: "予定登録に失敗しました。もう一度お試しください。"});
                     return;
                 }
 
                 res.json().then(newEvent => {
                     setEvents([ ...events, newEvent ]);
+                    setMsg({status: "info", content: `タイトル：${newEvent.title}の登録が完了しました。`});
+                    clearForm();
+                    return;
                 });
             })
     }
@@ -68,13 +89,13 @@ const Calender = () => {
                 <form method="POST" onSubmit={onSubmit}>
                     <div>
                         <label htmlFor="">タイトル</label>
-                        <input type="text" name="title" />
+                        <input type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} required />
                     </div>
                     <div>
                         {EVENT_TYPES.map((value) => {
                             return (
                             <label key={ value.id }>
-                                <input name="type" type="radio" value={ value.id } />
+                                <input name="type" type="radio" value={ value.id } onChange={e => setType(e.target.value)} checked={value.id == Number(type)} required />
                                 { value.name }
                             </label>
                             );
@@ -82,17 +103,18 @@ const Calender = () => {
                     </div>
                     <div>
                         <label htmlFor="">開始</label>
-                        <input type="datetime-local" name="start_at" />
+                        <input type="datetime-local" name="start_at" value={startAt} onChange={e => setStartAt(e.target.value)} required />
                     </div>
                     <div>
                         <label htmlFor="">終了</label>
-                        <input type="datetime-local" name="end_at" />
+                        <input type="datetime-local" name="end_at" value={endAt} onChange={e => setEndAt(e.target.value)} required />
                     </div>
                     <div>
                         <label htmlFor="">メモ</label>
-                        <input type="textarea" name="memo" />
+                        <input type="textarea" name="memo" value={memo} onChange={e => setMemo(e.target.value)} />
                     </div>
                     <button>登録</button>
+                    { msg && ( <div className={ `${ msg.status == "error" && "bg-red-100" } ${ msg.status == "info" && "bg-green-100" }`  }>{ msg.content }</div> ) }
                 </form>
             </div>
 
