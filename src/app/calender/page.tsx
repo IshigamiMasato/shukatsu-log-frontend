9e5508f05d4eaee4cf2be737680191fb1a556da8
M I <mi@emunoMacBook-Pro.local>
2025-02-02 11:45:13 +0900

    constantsディレクトリを作成

--- src/app/calender/page.tsx
+++ src/app/calender/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { Event } from "@/types";
+import { EVENT_TYPES } from "@/constants/eventConstants";
 
 const Calender = () => {
     useAuthInit();
@@ -38,15 +39,6 @@ const Calender = () => {
             getEvents();
         }
     }, [authStatusChecked]);
-
-    const EVENT_TYPES = [
-        { id: 1, name: "説明会" },
-        { id: 2, name: "リクルータ面談" },
-        { id: 3, name: "書類提出" },
-        { id: 4, name: "適性検査" },
-        { id: 5, name: "面談" },
-        { id: 6, name: "内定関連" },
-    ];
 
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
