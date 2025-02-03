50834cd54c8b55fcc9f2f741e87ff43fbd71c4e3
M I <mi@emunoMacBook-Pro.local>
2025-02-03 21:56:48 +0900

    ログイン時にバリデーションを表示

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,34 +1,53 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+import useAuthInit from "@/hooks/useAuthInit";
+import { RootState } from "@/store";
+import { redirect } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Login: React.FC = () => {
-    const [ error, setError ] = useState(null);
-    const router = useRouter();
+    /* 認証 */
+    useAuthInit();
+    const { isAuthenticated, user, authStatusChecked } = useSelector( (state: RootState) => state.auth );
+
+    const [error, setError] = useState<string | null>(null);
+    // バリデーションエラー用
+    const [errors, setErrors] = useState<{ email?: []; password?: []; }>({});
+
+    useEffect(() => {
+        if ( authStatusChecked ) {
+            // 認証状態確認後、認証済だった場合はユーザ画面へリダイレクト
+            if ( isAuthenticated ) {
+                redirect("/user");
+            }
+        }
+    }, [authStatusChecked]);
 
     const handleLogin = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         setError(null);
+        setErrors({});
 
         const form = e.target as HTMLFormElement;
         const formData = new FormData(form);
 
         fetch('api/login', {method: 'POST', body: formData}).then(res => {
             if ( ! res.ok ) {
-                res.json().then(data => {
-                    setError(data.msg);
-
-                    return;
+                res.json().then(res => {
+                    // バリデーションエラー
+                    if ( res.code == "BAD_REQUEST" ) {
+                        setErrors(res.errors);
+                    }
                 });
+                setError("ログインに失敗しました。");
+                return;
             }
 
             res.json().then(data => {
                 localStorage.setItem("access_token", data.access_token);
-                router.push("/user");
-
-                return;
+                redirect("/user");
             });
         })
     }
@@ -36,15 +55,17 @@ const Login: React.FC = () => {
     return (
         <div>
             <h1>ログイン</h1>
-            { error && <p>{ error }</p> }
+            { error && ( <div className="bg-red-100">{ error }</div> ) }
             <form onSubmit={handleLogin}>
                 <div>
                     <label>メールアドレス</label>
                     <input type="text" name="email" required />
+                    { errors.email && <p className="text-red-500">{ errors.email.join(',') }</p>}
                 </div>
                 <div>
                     <label>パスワード</label>
                     <input type="text" name="password" required />
+                    { errors.password && <p className="text-red-500">{ errors.password.join(',') }</p>}
                 </div>
                 <button>送信</button>
             </form>
