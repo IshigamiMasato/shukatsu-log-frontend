5e31bdb8b7903e317dfbf36e9fa5e16cd7981e0a
M I <mi@emunoMacBook-Pro.local>
2025-02-16 17:14:46 +0900

    応募一覧画面を追加

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -6,3 +6,12 @@ export const EVENT_TYPES = [
     { id: 5, name: "面談" },
     { id: 6, name: "内定関連" },
 ];
+
+export const APPLY_STATUS = [
+    { id: 1, name: "書類選考" },
+    { id: 2, name: "筆記試験等" },
+    { id: 3, name: "面接調整" },
+    { id: 4, name: "面接" },
+    { id: 5, name: "内定" },
+    { id: 6, name: "選考終了" },
+];
