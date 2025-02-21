3b565c5915ec3508a2005099c4c79b471942e662
M I <mi@emunoMacBook-Pro.local>
2025-02-21 23:24:43 +0900

    書類登録フォームを別コンポーネントへ切り出し

--- src/app/apply/[applyId]/process/create/page.tsx
+++ src/app/apply/[applyId]/process/create/page.tsx
@@ -1,83 +1,12 @@
 "use client";
 
-import { APPLY_STATUS } from "@/constants/const";
-import { dispToast } from "@/store/modules/toast";
-import moment from "moment";
-import { FormEvent, use, useState } from "react";
-import { useDispatch } from "react-redux";
+import DocumentForm from "@/components/DocumentForm";
+import { APPLY_STATUS, DOCUMENT_SELECTION } from "@/constants/const";
+import { use, useState } from "react";
 
-const fileToBase64 = async ( file : File ): Promise<string> => {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-
-        reader.onload = () => resolve(reader.result as string);
-        reader.onerror = () => reject(new Error('ファイル読み込み失敗'));
-
-        reader.readAsDataURL(file);
-    });
-}
-
-const ProcessCreatePage = ({ params } : { params : Promise<{ applyId: string }> }) => {
+const ProcessCreatePage = ({ params } : { params : Promise<{ applyId: number }> }) => {
     const { applyId } = use(params); // use()はawaitのように動作するため、use(params)の処理が完了するまでそれ以降の処理は実行されない
-
     const [status, setStatus] = useState<number>();
-
-    const FILE_COUNT = 5;
-
-    /* 書類 */
-    const [submissionDate, setSubmissionDate] = useState<string>( moment().format("YYYY-MM-DD") );
-    const [memo, setMemo] = useState<string>("");
-    const [files, setFiles] = useState<(File|undefined)[]>([]);
-
-    const [validationErrors, setValidationErrors] = useState<{ status?: []; submission_date?: []; file?: []; memo?: []; }>({});
-
-    const dispatch = useDispatch();
-
-    const onSubmit = async ( e: FormEvent<HTMLFormElement> ) => {
-        e.preventDefault();
-
-        setValidationErrors({});
-
-        const form = e.target as HTMLFormElement;
-        const formData = new FormData(form);
-
-        for (let i = 1; i <= FILE_COUNT; i++) {
-            try {
-                const file = files[i];
-
-                if ( file ) {
-                    const base64File = await fileToBase64(file);
-                    formData.append(`files[]`, base64File);
-                }
-
-            } catch ( error ) {
-                console.error(error);
-                dispatch( dispToast({ status: "error", message: "ファイルのアップロードに失敗しました。" }) );
-                return;
-            }
-        }
-
-        // 書類選考中
-        if ( status == 1 ) {
-            fetch(`/api/apply/${applyId}/document`, {
-                method: "POST",
-                body: formData
-            }).then(res => {
-                if( ! res.ok ) {
-                    res.json().then(res => {
-                        // バリデーションエラー
-                        if ( res.code == "BAD_REQUEST" ) {
-                            setValidationErrors(res.errors);
-                        }
-                    })
-                    dispatch( dispToast({ status: "error", message: `書類の登録に失敗しました。もう一度お試しください。` }) );
-                    return;
-                }
-
-                dispatch( dispToast({ status: "success", message: `書類の登録が完了しました。` }) );
-            })
-        }
-    }
 
     return (
         <>
@@ -97,54 +26,9 @@ const ProcessCreatePage = ({ params } : { params : Promise<{ applyId: string }> 
                         )
                     })}
                 </select>
-                { validationErrors.status && <p className="text-red-500">{ validationErrors.status.join(',') }</p> }
             </div>
 
-            { status == 1  && (
-                <form onSubmit={onSubmit} encType="multipart/form-data">
-                    <div>
-                        <label>書類提出日</label>
-                        <input
-                            type="date"
-                            name="submission_date"
-                            value={ submissionDate }
-                            onChange={ (e) => setSubmissionDate(e.target.value) }
-                        />
-                        { validationErrors.submission_date && <p className="text-red-500">{ validationErrors.submission_date.join(',') }</p> }
-                    </div>
-                    {
-                        [...Array(FILE_COUNT)].map((v, i) => {
-                            const index = i + 1; // indexを1スタートとする
-
-                            return (
-                                <div key={`file_${index}`}>
-                                    <label>提出書類</label>
-                                    <input
-                                        type="file"
-                                        name={`file_${index}`}
-                                        onChange={ (e) => {
-                                            const newFiles = [...files];
-                                            newFiles[index] = e.target.files?.[0];
-                                            setFiles(newFiles);
-                                        }}
-                                    />
-                                </div>
-                            )
-                        })
-                    }
-                    <div>
-                        <label>メモ</label>
-                        <input
-                            type="textarea"
-                            name="memo"
-                            value={ memo }
-                            onChange={ e => setMemo(e.target.value) }
-                        />
-                        { validationErrors.memo && <p className="text-red-500">{ validationErrors.memo.join(',') }</p> }
-                    </div>
-                    <button>登録</button>
-                </form>
-            )}
+            { status == DOCUMENT_SELECTION  && <DocumentForm applyId={applyId}/> }
 
             {status == 2  && (
                 <div>筆記試験選考中</div>
