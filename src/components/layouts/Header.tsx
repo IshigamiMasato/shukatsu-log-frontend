b1ed64bbbc75e9c0b5a273fbaf654951872a90ff
M I <mi@emunoMacBook-Pro.local>
2025-02-25 23:40:21 +0900

    ヘッダにスタイルを追加

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,6 +4,9 @@ import { RootState } from "@/store";
 import { useSelector } from "react-redux";
 import Button from "../elements/Button";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
     const { isAuthenticated } = useSelector((state: RootState) => state.auth);
@@ -20,15 +23,23 @@ const Header = () => {
     }
 
     return (
-        <div className="flex justify-between">
-            <h1>Header</h1>
-            <div>
-                { isAuthenticated
-                    ? ( <Button name="ログアウト" onClick={handleLogout} /> )
-                    : ( <Button name="ログイン" onClick={() => { redirect("/login") }} /> )
-                }
-            </div>
-        </div>
+        <header className="container mx-auto px-8 py-6 flex justify-between items-center">
+            <div className="text-lg font-bold">Shukatsu-Log</div>
+            { isAuthenticated && (
+                <>
+                    <div className="space-x-4 hidden md:flex items-center text-nowrap">
+                        <Link href="/calender">予定</Link>
+                        <Link href="/company">企業</Link>
+                        <Link href="/apply">応募</Link>
+                        <Link href="/user">会員情報</Link>
+                        <Button name="ログアウト" onClick={handleLogout} />
+                    </div>
+                    <div className="md:hidden">
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                </>
+            )}
+        </header>
     )
 }
 
