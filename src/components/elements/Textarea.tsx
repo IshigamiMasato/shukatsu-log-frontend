a8170178a02f350a739bc32d5b742bdbe9d39d0b
M I <mi@emunoMacBook-Pro.local>
2025-02-26 22:41:31 +0900

    企業編集画面にスタイルを適用

--- src/components/elements/Textarea.tsx
+++ src/components/elements/Textarea.tsx
@@ -2,13 +2,16 @@ type TextareaProps = {
     name: string,
     errors: string[]|undefined,
     rows?: number,
+    value?: string,
 } & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-const Textarea = ({ name, errors, rows } : TextareaProps) => {
+const Textarea = ({ name, errors, rows, value, ...props } : TextareaProps) => {
     return (
         <textarea
             name={name}
+            value={value}
             rows={rows ? rows : 4}
+            { ...props }
             className={ `bg-gray-50 border border-gray-300 rounded-lg text-sm p-2 w-full ${errors ? 'border-2 border-red-500' : ''}` }
         />
     )
