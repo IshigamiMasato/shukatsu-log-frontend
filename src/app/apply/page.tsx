b78c0bb6847a384952152c0abbb92858f251ed48
M I <mi@emunoMacBook-Pro.local>
2025-02-26 09:29:10 +0900

    応募一覧画面にスタイルを適用

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -18,41 +18,41 @@ const ApplyPage = async () => {
     const applies = await res.json();
 
     return (
-        <>
-            <Link href='/apply/create'>応募登録</Link>
-            <table>
-                <thead>
+        <div className="container mx-auto px-8 py-6">
+            <Link href='/apply/create' className="bg-blue-600 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-block mb-3">応募登録</Link>
+            <div className="overflow-x-auto shadow-md rounded-lg">
+            <table className="w-full text-sm text-left">
+                <thead className="text-xs bg-gray-50">
                     <tr>
-                        <th scope="col">応募ID</th>
-                        <th scope="col">企業名</th>
-                        <th scope="col">職種</th>
-                        <th scope="col">選考ステータス</th>
-                        <th scope="col">応募経路</th>
-                        <th scope="col">登録日時</th>
-                        <th scope="col">更新日時</th>
+                        <th scope="col" className="px-6 py-3 text-nowrap">ステータス</th>
+                        <th scope="col" className="px-6 py-3 text-nowrap">企業名</th>
+                        <th scope="col" className="px-6 py-3 text-nowrap">職種</th>
+                        <th scope="col" className="px-6 py-3 text-nowrap">応募経路</th>
+                        <th scope="col" className="px-6 py-3 text-nowrap">登録日時</th>
+                        <th scope="col" className="px-6 py-3 text-nowrap">更新日時</th>
                     </tr>
                 </thead>
                 <tbody>
                     {applies.map((apply: Apply) => {
                         return (
-                            <tr key={ apply.apply_id }>
-                                <td>
-                                    <Link href={`/apply/${apply.apply_id}`}>{ apply.apply_id }</Link>
+                            <tr key={ apply.apply_id } className="bg-white border-b border-gray-200 hover:bg-gray-50">
+                                <td className="px-6 py-3 font-medium whitespace-nowrap">
+                                    <Link href={`/apply/${apply.apply_id}`} className="border p-1 rounded-lg text-blue-500 hover:bg-blue-100 transition-all duration-300">{ APPLY_STATUS.find(status => status.id == apply.status)?.name }</Link>
                                 </td>
-                                <td>
-                                    <Link href={`/company/${apply.company_id}`}>{ apply.company.name }</Link>
+                                <td className="px-6 py-3 font-medium whitespace-nowrap">
+                                    <Link href={`/company/${apply.company_id}`} className="text-blue-500 hover:underline">{ apply.company.name }</Link>
                                 </td>
-                                <td>{ apply.occupation }</td>
-                                <td>{ APPLY_STATUS.find(status => status.id == apply.status)?.name }</td>
-                                <td>{ apply.apply_route }</td>
-                                <td>{ apply.created_at }</td>
-                                <td>{ apply.updated_at }</td>
+                                <td className="px-6 py-3 font-medium whitespace-nowrap">{ apply.occupation }</td>
+                                <td className="px-6 py-3 font-medium whitespace-nowrap">{ apply.apply_route }</td>
+                                <td className="px-6 py-3 font-medium whitespace-nowrap">{ apply.created_at }</td>
+                                <td className="px-6 py-3 font-medium whitespace-nowrap">{ apply.updated_at }</td>
                             </tr>
                         );
                     })}
                 </tbody>
             </table>
-        </>
+            </div>
+        </div>
     )
 }
 
