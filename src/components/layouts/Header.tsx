b9201ff42bef76a20e591a953339e23ed08f6b7a
M I <mi@emunoMacBook-Pro.local>
2025-03-02 12:46:35 +0900

    ログアウトボタンを修正

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -14,18 +14,22 @@ const Header = () => {
     const { isAuthenticated } = useSelector((state: RootState) => state.auth);
 
     const handleLogout = () => {
-        fetch('/api/logout', {
-            method: "POST",
-        }).then(res => {
-            if ( ! res.ok ) {
-                res.json().then(data => console.error(data));
-            }
-            redirect("/login");
-        });
+        const isConfirmed = window.confirm("ログアウトしますか？");
+
+        if ( isConfirmed ) {
+            fetch('/api/logout', {
+                method: "POST",
+            }).then(res => {
+                if ( ! res.ok ) {
+                    res.json().then(data => console.error(data));
+                }
+                redirect("/login");
+            });
+        }
     }
 
     return (
-        <header className="border mb-16">
+        <header className="border mb-8">
             <div className="container mx-auto px-8 py-6">
                 <div className="flex justify-between items-center">
                     <div className="text-lg font-bold">Shukatsu-Log</div>
@@ -37,8 +41,8 @@ const Header = () => {
                                 <Link href="/apply">応募</Link>
                                 <Link href="/user">会員情報</Link>
                                 <Button onClick={handleLogout}>
+                                    <FontAwesomeIcon icon={faRightFromBracket} className="mr-1" />
                                     ログアウト
-                                    <FontAwesomeIcon icon={faRightFromBracket} />
                                 </Button>
                             </div>
                             <div className="md:hidden">
@@ -58,8 +62,8 @@ const Header = () => {
                             <li><Link href="/user" className="block font-medium text-sm px-5 py-2.5 hover:bg-gray-50 border-b">会員情報</Link></li>
                             <li>
                                 <Button onClick={handleLogout} className="hover:bg-gray-50 w-full !text-left">
+                                    <FontAwesomeIcon icon={faRightFromBracket} className="mr-1" />
                                     ログアウト
-                                    <FontAwesomeIcon icon={faRightFromBracket} />
                                 </Button>
                             </li>
                         </ul>
