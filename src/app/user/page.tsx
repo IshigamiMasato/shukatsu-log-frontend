6811a6530915e6035883df53a76ab860c4cb2c8d
M I <mi@emunoMacBook-Pro.local>
2025-03-08 16:58:49 +0900

    ユーザ取得処理をfeaturesへ移行

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,38 +1,4 @@
-import { getJWT } from "@/helper";
-import { User } from "@/types";
-import { notFound, redirect } from "next/navigation";
-
-const getUser = async (): Promise<User|null|never> => {
-    const jwt = await getJWT();
-
-    const res = await fetch(`http://backend/api/user`, {
-        method: "GET",
-        headers: { Authorization: `Bearer ${jwt}` }
-    });
-
-    if ( ! res.ok ) {
-        const data = await res.json();
-
-        if ( res.status == 401 ) {
-            // トークン有効期限切れはフロントのクライアントコンポーネント側でリフレッシュ処理を実施し、再チャレンジするため特にエラーは返さずNULL返却
-            if ( data.code === "EXPIRED_TOKEN" ) {
-                return null;
-            }
-
-            redirect('/login');
-        }
-
-        if ( res.status == 404 ) {
-            notFound();
-        }
-
-        throw new Error( `Failed fetch user. (status=${res.status}, data=${JSON.stringify(data)})` );
-    }
-
-    const user: User = await res.json();
-
-    return user;
-}
+import { getUser } from "@/features/user/api/getUser";
 
 const UserPage = async () => {
     const user = await getUser();
