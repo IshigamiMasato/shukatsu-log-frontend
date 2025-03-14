519ba96b241d5b2ba633ebb9305c0425e0562221
M I <mi@emunoMacBook-Pro.local>
2025-03-14 22:31:37 +0900

    404ページを修正

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,19 @@
+import Button from "@/components/elements/Button";
+import Link from "next/link";
+
 const NotFound = () =>  {
     return (
-        <div>Not Found</div>
-    );
+        <div className="w-full sm:max-w-lg max-w-sm p-16 bg-white mx-auto rounded-lg border">
+            <h2 className="text-3xl font-bold mb-3">404 Not Found</h2>
+            <p className="text-sm">ご指定のページが存在しませんでした。</p>
+            <p className="text-sm">URLに問題があるか、ページが削除された可能性があります。</p>
+            <Link href='/'>
+                <Button className="bg-blue-700 hover:bg-blue-800 text-white mt-3">
+                    トップページへ戻る
+                </Button>
+            </Link>
+        </div>
+    )
 }
 
 export default NotFound;
